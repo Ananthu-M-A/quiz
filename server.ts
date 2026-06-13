4cbb45f1691c72b97609874a2176e2ae57fd0997
Ananthu-M-A <ananthumapookkad@gmail.com>
2026-06-13 23:01:40 +0530

    feat: Implemented Server Setup with Socket.IO for Quiz Session Management

---
+++ server.ts
@@ -0,0 +1,107 @@
+import { createServer } from "http";
+import next from "next";
+import { Server } from "socket.io";
+
+import { nanoid } from "nanoid";
+
+import { quizStore } from "./lib/quizStore";
+
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev });
+
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+
+  const httpServer = createServer(
+    (req, res) => {
+      handler(req, res);
+    }
+  );
+
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*"
+    }
+  });
+
+  io.on("connection", (socket) => {
+
+    console.log(
+      "connected",
+      socket.id
+    );
+
+    socket.on(
+      "create-session",
+      (questions) => {
+
+        const code = nanoid(6);
+
+        quizStore[code] = {
+          code,
+          status: "waiting",
+          questions,
+          participants: [],
+          answers: [],
+          currentQuestionIndex: 0
+        };
+
+        socket.emit(
+          "session-created",
+          code
+        );
+      }
+    );
+
+    socket.on(
+      "join-session",
+      ({
+        code,
+        name
+      }) => {
+
+        const session =
+          quizStore[code];
+
+        if (!session) {
+          socket.emit(
+            "error-message",
+            "Invalid Quiz Code"
+          );
+          return;
+        }
+
+        const participant = {
+          id: nanoid(),
+          name,
+          score: 0,
+          totalTime: 0
+        };
+
+        session.participants.push(
+          participant
+        );
+
+        socket.join(code);
+
+        io.to(code).emit(
+          "participants-updated",
+          session.participants
+        );
+      }
+    );
+
+  });
+
+  httpServer.listen(
+    3000,
+    () => {
+      console.log(
+        "Running on port 3000"
+      );
+    }
+  );
+
+});
